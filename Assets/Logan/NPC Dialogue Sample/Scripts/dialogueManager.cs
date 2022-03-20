16b8f19c62e9c6e241b675af9097c367e492f2b5
logancox987 <logancox987@gmail.com>
2022-03-20 15:21:59 -0700

    Re-implemented Leo's fix

--- Assets/Logan/NPC Dialogue Sample/Scripts/dialogueManager.cs
+++ Assets/Logan/NPC Dialogue Sample/Scripts/dialogueManager.cs
@@ -9,7 +9,7 @@ public class dialogueManager : MonoBehaviour
 {
 
     [SerializeField] GameObject dialogueBox;
-    [SerializeField]  GameObject dialogueTextBox;
+    [SerializeField] GameObject dialogueTextBox;
     public TextMeshProUGUI dialogueText;
     [SerializeField] int lettersPerSecond;
     public GameObject player;
@@ -31,7 +31,7 @@ public class dialogueManager : MonoBehaviour
         {
 
             ++currentLine;
-            if(currentLine < dialogue.Lines.Count)
+            if (currentLine < dialogue.Lines.Count)
             {
                 StartCoroutine(TypeDialogue(dialogue.Lines[currentLine]));
             }
@@ -47,12 +47,60 @@ public class dialogueManager : MonoBehaviour
 
     public IEnumerator ShowDialogue(dialogue dialogue)
     {
-            yield return new WaitForEndOfFrame();
-            
-            this.dialogue = dialogue;
-            dialogueBox.SetActive(true);
-            StartCoroutine(TypeDialogue(dialogue.Lines[0]));
+        yield return new WaitForEndOfFrame();
+
+        this.dialogue = dialogue;
+        dialogueBox.SetActive(true);
+        StartCoroutine(TypeDialogue(dialogue.Lines[0]));
     }
+
+    // New TypeDialogue Method With Skip Ability 
+    public IEnumerator TypeDialogue(string dialogue)
+    {
+        isTyping = true;
+        dialogueText.text = dialogue;
+        int totalVisibleCharacters = dialogue.ToString().Length;
+        int counter = 0;
+
+        float delayTime = 0.03f;
+        float nextTime = Time.time;
+
+
+        foreach (var letter in dialogue.ToCharArray())
+        {
+            while (true)
+            {
+                // Wait for delay to continue
+                if (Time.time > nextTime)
+                {
+                    nextTime = Time.time + delayTime;
+                    int visibleCount = counter % (totalVisibleCharacters + 1);
+                    dialogueText.maxVisibleCharacters = visibleCount;
+                    if (visibleCount >= totalVisibleCharacters)
+                    {
+                        break;
+                    }
+                    counter++;
+                    break;
+                }
+                yield return null;
+
+                //Check for skip input
+                if (playerInput.actions["Interact"].triggered)
+                {
+                    dialogueText.maxVisibleCharacters = totalVisibleCharacters;
+                    // Exits both loops
+                    goto exitLoops;
+                }
+            }
+        }
+
+        exitLoops:
+        isTyping = false;
+    }
+
+
+    // Original TypeDialogue Method 
 
     //public IEnumerator TypeDialogue(string dialogue)
     //{
@@ -62,71 +110,18 @@ public class dialogueManager : MonoBehaviour
     //    int totalVisibleCharacters = dialogue.ToString().Length;
     //    int counter = 0;
 
-    //    //float timer = 0;
-    //    float delayTime = 0.03f;
-    //    float nextTime = Time.time + delayTime;
-
-
     //    foreach (var letter in dialogue.ToCharArray())
     //    {
-    //        //Debug.Log("Char: " + counter);
-    //        while (true)
+    //        int visibleCount = counter % (totalVisibleCharacters + 1);
+    //        dialogueText.maxVisibleCharacters = visibleCount;
+    //        if (visibleCount >= totalVisibleCharacters)
     //        {
-    //            // Wait for delay to continue
-    //            if (Time.time > nextTime)
-    //            {
-    //                //timer = 0;
-    //                nextTime = Time.time + delayTime;
-    //                int visibleCount = counter % (totalVisibleCharacters + 1);
-    //                if (visibleCount >= totalVisibleCharacters)
-    //                {
-    //                    break;
-    //                }
-    //                dialogueText.maxVisibleCharacters = visibleCount;
-    //                counter++;
-    //                break;
-    //            }
-    //            yield return null;
-
-    //            // Increment timer
-    //            //timer += Time.deltaTime;
-
-    //            //Check for skip input
-    //            if (Input.GetKeyDown(KeyCode.A))
-    //            {
-    //                Debug.Log("Skip");
-    //                dialogueText.maxVisibleCharacters = totalVisibleCharacters;
-    //                isTyping = false;
-    //                break;
-    //            }
+    //            isTyping = false;
     //        }
+    //        counter += 1;
+    //        yield return new WaitForSeconds(0.03f);
     //    }
-    //    Debug.Log("exited");
+
     //    isTyping = false;
     //}
-
-    public IEnumerator TypeDialogue(string dialogue)
-    {
-        isTyping = true;
-        dialogueText.text = dialogue;
-
-        int totalVisibleCharacters = dialogue.ToString().Length;
-        int counter = 0;
-
-        foreach (var letter in dialogue.ToCharArray())
-        {
-            int visibleCount = counter % (totalVisibleCharacters + 1);
-            dialogueText.maxVisibleCharacters = visibleCount;
-            if (visibleCount >= totalVisibleCharacters)
-            {
-                isTyping = false;
-            }
-            counter += 1;
-
-
-            yield return new WaitForSeconds(0.03f);
-        }
-
-        isTyping = false;
-    }
 }
