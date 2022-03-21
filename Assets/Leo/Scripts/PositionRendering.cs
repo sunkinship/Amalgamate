42e7da6caccc1132295cf9f876e1dfd1cce3dbf4
Leo <leo_1517@outlook.com>
2022-03-20 20:39:49 -0700

    Added lights and player in tech demo scene

---
+++ Assets/Leo/Scripts/PositionRendering.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendering : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    private Renderer myRenderer;
+
+    private void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y);
+    }
+}
