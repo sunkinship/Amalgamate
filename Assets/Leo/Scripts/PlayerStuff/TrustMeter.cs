352809795d4d40af511d499f0344e0caf62af7fa
Leo <leo_1517@outlook.com>
2022-03-24 10:19:45 -0700

    Worked on trust meter

---
+++ Assets/Leo/Scripts/PlayerStuff/TrustMeter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrustMeter : MonoBehaviour
+{
+    private Slider slider;
+
+    public float fillSpeed = 10;
+    public float targetProgress;
+    public float target = 0;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        AddProgress(1f);
+    }
+
+    private void Update()
+    {
+        if (slider.value < targetProgress)
+            slider.value += fillSpeed * Time.deltaTime;
+    }
+
+    public void AddProgress(float newProgress)
+    {
+        targetProgress = slider.value + newProgress;
+    }
+}
