79b21e37943ebc6e05bf8a20d5fc58e1cdc722c6
DESKTOP-H2GALKD\Sowmya <1710136@mukilteo.wednet.edu>
2022-03-24 20:37:48 -0700

    Fast travel working and fade transition kinda working

--- Assets/Shreyas/fastTravel.cs
+++ Assets/Shreyas/fastTravel.cs
@@ -4,30 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class fastTravel : MonoBehaviour
 {
-    public Image black;
     public Animator anim;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    public void OngoToHomeClick()
-    {
-        StartCoroutine(Fading());
-    }
-
-    IEnumerator Fading()
+    public void OnClickGoHome()
     {
         anim.SetBool("Fade", true);
-        yield return new WaitUntil(() => black.color.a == 1);
+        StartCoroutine(Fade());
         SceneManager.LoadScene("Home", LoadSceneMode.Single);
     }
+
+    IEnumerator Fade()
+    {
+        yield return new WaitForSeconds(1.5f);
+    }
+
 }
