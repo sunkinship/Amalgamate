626c16b882661e7044b6e9b998bc30a12a891b73
logancox987 <logancox987@gmail.com>
2022-03-27 00:43:40 -0700

    Messed with the start of puzzle mechanics

---
+++ Assets/Logan/Scripts/player/pushPullObjects.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class pushPullObjects : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public GameObject heldItem;
+    public GameObject CurrentInteractiveZone;
+    public GameObject player;
+    public GameObject leftInterZone;
+    public GameObject rightInterZone;
+    public GameObject downInterZone;
+    public GameObject upInterZone;
+    public bool isPulling;
+
+
+    public void Start()
+    {
+        playerInput = player.GetComponent<PlayerInput>();
+    }
+
+    public void Update()
+    {
+        if(player.GetComponent<playerMovement>().lastFacingDirection == "RIGHT" && isPulling == false)
+        {
+            CurrentInteractiveZone = rightInterZone;
+        }
+        if (player.GetComponent<playerMovement>().lastFacingDirection == "LEFT" && isPulling == false)
+        {
+            CurrentInteractiveZone = leftInterZone;
+        }
+        if (player.GetComponent<playerMovement>().lastFacingDirection == "UP" && isPulling == false)
+        {
+            CurrentInteractiveZone = upInterZone;
+        }
+        if (player.GetComponent<playerMovement>().lastFacingDirection == "DOWN" && isPulling == false)
+        {
+            CurrentInteractiveZone = downInterZone;
+        }
+    }
+
+}
