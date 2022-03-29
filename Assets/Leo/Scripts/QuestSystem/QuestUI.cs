128feddfb312b0b3efce6208f63a29ef08e6d068
Leo <leo_1517@outlook.com>
2022-03-29 10:16:08 -0700

    Started on quest list ui

---
+++ Assets/Leo/Scripts/QuestSystem/QuestUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestUI : MonoBehaviour
+{
+    public GameObject panel;
+    public TextMeshProUGUI listText;
+    public List<Quest> questList;
+    private string textContents;
+
+    private void Start()
+    {
+        textContents = "       Quests\n";    
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            panel.SetActive(true);
+        } 
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Updates contents of the quest list UI
+    /// </summary>
+    private void UpdateList()
+    {
+        foreach (Quest quest in questList)
+        {
+            textContents += "*" + quest.name + "\n  -" + quest.description + "\n";
+            if (quest.isActive && quest.isComplete == false)
+            {
+                textContents += "*" + quest.name + "\n  -" + quest.description + "\n";
+            }
+            listText.text = textContents;
+        }
+    }
+}
