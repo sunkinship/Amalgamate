74a2778bdf40f55551a5ae159a1c97816f6567d8
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2022-04-19 08:42:12 -0700

    fhyjmngjfyj

---
+++ Assets/Logan/Scripts/door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class door : MonoBehaviour
+{
+    public string sceneToLoad;
+    public bool mazeExit;
+    public GameObject player;
+    public Animator anim;
+    public PlayerInput playerInput;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" && playerInput.actions["Interact"].triggered)
+        {
+            player = collision.gameObject;
+
+            if (mazeExit == true)
+            {
+                StartCoroutine(Fade());
+                //SceneManager.LoadScene(sceneToLoad);
+                collision.gameObject.GetComponent<playerMovement>().goToMazeExit = true;
+
+            }
+            else
+            {
+                //SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Fade());
+            }
+        }
+
+    }
+
+    public IEnumerator Fade()
+    {
+        anim.SetTrigger("FadeTrigger");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
