39702e4c2859a8ee9400e0341578678167cc2d13
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2022-04-20 09:10:26 -0700

    Timed buttons

---
+++ Assets/Logan/Scripts/puzzleButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class puzzleButton : MonoBehaviour
+{
+    public GameObject itemToDisable;
+    public float time;
+    public GameObject player;
+    public PlayerInput playerInput;
+    public bool canPressButton;
+
+
+
+    private void Update()
+    {
+        if(canPressButton == true && playerInput.actions["Interact"].triggered)
+        {
+            StartCoroutine(buttonWork());
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            canPressButton = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canPressButton = false;
+        }
+    }
+
+    public IEnumerator buttonWork()
+    {
+        itemToDisable.SetActive(false);
+        yield return new WaitForSeconds(time);
+        itemToDisable.SetActive(true);
+    }
+}
