976b267d49218158eadfd0a312c6970f6b2f3eec
Leo <leo_1517@outlook.com>
2022-04-20 23:57:22 -0700

    Implemented walking animation when entering loading zones and leaves into main scenes

--- Assets/Leo/Scripts/PlayerStuff/EnterLoadingZone.cs
+++ Assets/Leo/Scripts/PlayerStuff/EnterLoadingZone.cs
@@ -7,9 +7,15 @@ public class EnterLoadingZone : MonoBehaviour
     public GameObject player;
     public Vector3 targetPos;
     public float speed;
+    [HideInInspector]
     public Animator ani;
     private bool moving;
+    public string playerDirection;
 
+    private void Awake()
+    {
+        ani = player.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -30,13 +36,38 @@ public class EnterLoadingZone : MonoBehaviour
         while (player.transform.position != targetPos)
         {
             //Debug.Log("move");
-            ani.SetBool("isMoving", true);
-            ani.SetBool("Right", true);
-            ani.SetBool("isCarrying", false);
+            SetDirection();
             player.transform.position = Vector2.MoveTowards(player.transform.position, targetPos, speed * Time.deltaTime);
             yield return null;
         }
         moving = false;
         player.GetComponent<playerMovement>().inLoadingZone = false;
+    }
+
+    private void SetDirection()
+    {
+        switch (playerDirection)
+        {
+            case "Left":
+                ani.SetBool("isMoving", true);
+                ani.SetBool("Left", true);
+                ani.SetBool("isCarrying", false);
+                break;
+            case "Right":
+                ani.SetBool("isMoving", true);
+                ani.SetBool("Right", true);
+                ani.SetBool("isCarrying", false);
+                break;
+            case "Up":
+                ani.SetBool("isMoving", true);
+                ani.SetBool("Up", true);
+                ani.SetBool("isCarrying", false);
+                break;
+            case "Down":
+                ani.SetBool("isMoving", true);
+                ani.SetBool("Down", true);
+                ani.SetBool("isCarrying", false);
+                break;
+        }
     }
 }
