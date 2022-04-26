d47029802b44ad3b2cbd9e0c15478237e874f1a5
MUKILTEO\1710194 <1710194@mukilteo.wednet.edu>
2022-04-26 09:42:13 -0700

    i want to cry ;(

---
+++ Assets/Tyler/YoinkedScripts/MirrorInteractions.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MirrorInteractions : MonoBehaviour
+{
+    private bool isKeyDown;
+    private bool isFacingInteractable;
+    public PlayerInput playerInput;
+    public bool isInteractingWithNPC;
+
+    private GameObject currentNPC;
+
+    public string nameString;
+    public Text nameText;
+    public GameObject npcPortrait;
+    public GameObject forPortrait;
+
+    public GameObject player;
+    public ItemGiver npcItem;
+    private QuestGiver npcQuest;
+    private PlayerManager playerManager;
+
+
+
+
+    private void Awake()
+    {
+        playerManager = player.GetComponent<PlayerManager>();
+        //playerInput = gameObject.GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        if (currentNPC != null)
+        {
+            npcPortrait.GetComponent<Image>().sprite = currentNPC.GetComponent<npcInteract>().currentPortrait;
+        }
+
+        if (isFacingInteractable == true)
+        {
+            player.GetComponent<MirrorPlayer>().isFacingNPC = true;
+        }
+        else
+        {
+            player.GetComponent<MirrorPlayer>().isFacingNPC = false;
+        }
+
+        // Initial check for interaction -> npcInteract -> dialogueManager
+        isKeyDown = playerInput.actions["Interact"].triggered;
+
+        // Regular dialogue 
+        if (isKeyDown && isFacingInteractable == true && player.GetComponent<MirrorPlayer>().inDialogue == false && player.GetComponent<MirrorPlayer>().speakCooldownLeft < 0 && player.GetComponent<MirrorPlayer>().carryingObject == false)
+        {
+            RegularInteraction();
+        }
+        // Forced dialogue 
+        else if (isFacingInteractable == true && currentNPC.GetComponent<npcInteract>().forceDialogue == true && currentNPC.GetComponent<npcInteract>().mustHoldCertainObject == false)
+        {
+            RegularForecedInteraction();
+        }
+        // Foreced dialogue requiring held object
+        else if (isFacingInteractable == true && currentNPC.GetComponent<npcInteract>().forceDialogue == true && currentNPC.GetComponent<npcInteract>().mustHoldCertainObject == true)
+        {
+            HoldingObjectInteraction();
+        }
+
+    }
+
+
+    /// <summary>
+    /// Initiates regular dialogue 
+    /// </summary>
+    private void RegularInteraction()
+    {
+        nameString = currentNPC.GetComponent<npcInteract>().NPCName;
+
+        forPortrait.GetComponent<managePortraits>().currentNPC = currentNPC;
+        //currentNPC.GetComponent<interactable>()?.Interact(CheckQuestState());
+        CheckQuestState();
+        nameText.text = nameString;
+        player.GetComponent<MirrorPlayer>().rb2.velocity = new Vector2(0, 0);
+    }
+
+
+    /// <summary>
+    /// Initiates forced dialogue
+    /// </summary>
+    private void RegularForecedInteraction()
+    {
+        nameString = currentNPC.GetComponent<npcInteract>().NPCName;
+
+        forPortrait.GetComponent<managePortraits>().currentNPC = currentNPC;
+        //currentNPC.GetComponent<interactable>()?.Interact(CheckQuestState());
+        currentNPC.GetComponent<npcInteract>().UpdateForcedColliders();
+        PlayerManager.forcedDialogueEncounters.Add(currentNPC.name);
+        CheckQuestState();
+        nameText.text = nameString;
+        player.GetComponent<MirrorPlayer>().rb2.velocity = new Vector2(0, 0);
+    }
+
+
+    /// <summary>
+    /// Initiates forced dialogue requiring held object
+    /// </summary>
+    private void HoldingObjectInteraction()
+    {
+        if (player.GetComponent<MirrorPlayer>().carryingObject == true)
+        {
+            nameString = currentNPC.GetComponent<npcInteract>().NPCName;
+
+            forPortrait.GetComponent<managePortraits>().currentNPC = currentNPC;
+            //currentNPC.GetComponent<interactable>()?.Interact(CheckQuestState());
+            currentNPC.GetComponent<npcInteract>().UpdateForcedColliders();
+            PlayerManager.forcedDialogueEncounters.Add(currentNPC.name);
+            CheckQuestState();
+            nameText.text = nameString;
+            player.GetComponent<MirrorPlayer>().rb2.velocity = new Vector2(0, 0);
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// Set portrait based on quest state
+    /// </summary>
+    private void CheckQuestState()
+    {
+        npcQuest = currentNPC.GetComponent<QuestGiver>();
+        npcItem = currentNPC.GetComponent<ItemGiver>();
+
+        // Check current state of quest
+        playerManager.ProgressQuest(npcQuest);
+
+        // Check if player has received linked quest
+        npcItem.CheckToGiveItem();
+
+        if (npcQuest.quest.hasPreConditionQuest)
+        {
+            // Check if player has finsished pre condition quest
+            npcQuest.CheckforPreCondition();
+            PreConditionCheck();
+        }
+        else if (npcQuest.quest.hasConnectedQuest)
+        {
+            //Check if player has met pre-condition for connected quest
+            npcQuest.CheckforConnectedQuest();
+            ConnectedQuestCheck();
+        }
+        else
+        {
+            RegularQuestCheck();
+        }
+    }
+
+    /// <summary>
+    /// Checks quest state for regular quests
+    /// </summary>
+    private void RegularQuestCheck()
+    {
+        //Linked quest triggered
+        if (npcItem.canGiveItem == true)
+        {
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsLinkedQuest[0], "Linked");
+        }
+        // No available quest
+        else if (npcQuest.quest.hasQuest == false)
+        {
+            //Debug.Log("no quest");
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsNoQuest[0], "None");
+        }
+        // Regular quest not started
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPreQuest[0], "New");
+        }
+        // Quest started but not completed
+        else if (npcQuest.quest.isActive && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsMidQuest[0], "Started");
+        }
+        // First interaction after quest completed
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete && npcQuest.quest.isPostQuest == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostQuest[0], "Complete");
+        }
+        // Post quest complete
+        else
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostPostQuest[0], "Post");
+        }
+    }
+
+    /// <summary>
+    /// Checks quest state for connected quests
+    /// </summary>
+    private void ConnectedQuestCheck()
+    {
+        //Linked quest triggered
+        if (npcItem.canGiveItem == true)
+        {
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsLinkedQuest[0], "Linked");
+        }
+        // Connected quest not received yet
+        else if (npcQuest.quest.isConnectedQuest == false)
+        {
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsNoQuest[0], "None");
+        }
+        // Quest not started
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("quest not started?");
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPreQuest[0], "New");
+        }
+        // Quest started but not completed
+        else if (npcQuest.quest.isActive && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsMidQuest[0], "Started");
+        }
+        // First interaction after quest completed
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete && npcQuest.quest.isPostQuest == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostQuest[0], "Complete");
+        }
+        // Post quest complete
+        else
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostPostQuest[0], "Post");
+        }
+    }
+
+    /// <summary>
+    /// Checks quest state for pre condition quests
+    /// </summary>
+    private void PreConditionCheck()
+    {
+        //Linked quest triggered
+        if (npcItem.canGiveItem == true)
+        {
+            //return currentNPC.GetComponent<npcInteract>().portraitsLinkedQuest[0];
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsLinkedQuest[0], "Linked");
+        }
+        // Connected quest not received yet
+        else if (npcQuest.quest.isAvailable == false)
+        {
+            //Debug.Log("not ready:  " + npcQuest.quest.isConnectedQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsNoQuest[0], "None");
+        }
+        // Quest not started
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPreQuest[0], "New");
+        }
+        // Quest started but not completed
+        else if (npcQuest.quest.isActive && npcQuest.quest.isComplete == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsMidQuest[0], "Started");
+        }
+        // First interaction after quest completed
+        else if (npcQuest.quest.isActive == false && npcQuest.quest.isComplete && npcQuest.quest.isPostQuest == false)
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostQuest[0], "Complete");
+        }
+        // Post quest complete
+        else
+        {
+            //Debug.Log("isActive: " + npcQuest.quest.isActive + ". isComplete: " + npcQuest.quest.isComplete + ". isPostQuest " + npcQuest.quest.isPostQuest);
+            currentNPC.GetComponent<interactable>()?.Interact(currentNPC.GetComponent<npcInteract>().portraitsPostPostQuest[0], "Post");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "interactableNPC")
+        {
+            isFacingInteractable = true;
+
+            currentNPC = collision.gameObject;
+
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isFacingInteractable = false;
+    }
+}
---
+++ Assets/Tyler/YoinkedScripts/MirrorRock.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class MirrorRock : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public static GameObject heldItem;
+    public GameObject player;
+    private bool isKeyDown;
+    private bool isFacingMovable;
+    public static GameObject currentMovable;
+    public static bool isMovingObject;
+    public static bool canDropObject;
+    static bool canPickUp;
+
+    private static Collider2D heldItemCollider;
+
+    [SerializeField] private LayerMask wallLayerMask;
+
+    private static Renderer heldObjectRender;
+    private static Renderer playerRender;
+
+    private static Vector3 direction;
+
+    public Animator ani;
+
+    private void Awake()
+    {
+        playerRender = player.GetComponent<Renderer>();
+        playerInput = player.GetComponent<PlayerInput>();
+        canPickUp = true;
+    }
+
+
+    public void Update()
+    {
+        //Pick up
+        if (playerInput.actions["Interact"].triggered && isFacingMovable == true && isMovingObject == false && canPickUp == true && player.GetComponent<MirrorPlayer>().inDialogue == false)
+        {
+            //Debug.Log("picked up");
+            ani.SetBool("isCarrying", true);
+            canPickUp = false;
+            player.GetComponent<MirrorPlayer>().moveSpeed = 2.5f;
+            heldObjectRender = heldItem.GetComponent<Renderer>();
+            heldObjectRender.GetComponent<PositionRendering>().enabled = false;
+            heldItem.GetComponent<BoxCollider2D>().enabled = false;
+            heldItemCollider = heldItem.GetComponent<BoxCollider2D>();
+            canDropObject = false;
+            isMovingObject = true;
+            player.GetComponent<MirrorPlayer>().carryingObject = true;
+            Invoke("enableDrop", 0.2f);
+
+        }
+        //Drop
+        if (playerInput.actions["Interact"].triggered && isMovingObject == true && canDropObject == true && player.GetComponent<MirrorPlayer>().inDialogue == false && player.GetComponent<MirrorPlayer>().isFacingNPC == false)
+        {
+            heldItem.GetComponentInChildren<ParticleSystem>().Play();
+            ani.SetBool("isCarrying", false);
+            canPickUp = false;
+            heldObjectRender.GetComponent<PositionRendering>().enabled = true;
+            heldItem.GetComponent<BoxCollider2D>().enabled = true;
+            isMovingObject = false;
+            currentMovable.transform.position = this.gameObject.transform.position;
+            player.GetComponent<MirrorPlayer>().carryingObject = false;
+            currentMovable = null;
+            heldItem = null;
+            canDropObject = false;
+            player.GetComponent<MirrorPlayer>().moveSpeed = 6;
+            Invoke("CanPickUp", 0.2f);
+        }
+
+
+        if (isMovingObject == true)
+        {
+            heldObjectRender.sortingOrder = playerRender.sortingOrder + 100;
+            currentMovable.transform.position = player.transform.position + currentMovable.GetComponent<holdableObject>().holdLocation;
+
+            if (player.GetComponent<MirrorPlayer>().inDialogue == true)
+            {
+                canDropObject = false;
+            }
+
+            //if (Physics2D.BoxCast(heldItemCollider.bounds.center, heldItemCollider.bounds.size, 0f, direction, 3f, wallLayerMask))
+            //{
+            //    Debug.Log("false");
+            //    canDrop = false;
+            //}
+            //else
+            //{
+            //    Debug.Log("TRUE" + Physics2D.BoxCast(heldItemCollider.bounds.center, heldItemCollider.bounds.size, 0f, direction, 3000f, wallLayerMask));
+            //    canDrop = true;
+            //}
+
+        }
+    }
+
+    private void enableDrop()
+    {
+        canDropObject = true;
+    }
+
+    private void CanPickUp()
+    {
+        canPickUp = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "movableObject" && isMovingObject == false)
+        {
+            isFacingMovable = true;
+            heldItem = collision.gameObject;
+            currentMovable = collision.gameObject;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "movableObject")
+        {
+            isFacingMovable = false;
+        }
+
+    }
+}
