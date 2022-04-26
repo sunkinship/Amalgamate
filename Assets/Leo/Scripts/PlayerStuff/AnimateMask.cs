93f8a8a84a5d7aa351ac93ec9153c349b8bbfa43
Leo <leo_1517@outlook.com>
2022-04-26 10:22:58 -0700

    Shadow testing

---
+++ Assets/Leo/Scripts/PlayerStuff/AnimateMask.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMask : MonoBehaviour
+{
+    public SpriteMask mask;
+
+    public SpriteRenderer targetRenderer;
+
+    private void LateUpdate()
+    {
+        if (mask.sprite != targetRenderer)
+        {
+            mask.sprite = targetRenderer.sprite;
+        }
+    }
+}
