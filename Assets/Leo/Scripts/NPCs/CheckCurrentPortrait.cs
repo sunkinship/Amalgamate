05f1fb5cdd8ad397c327dc698b9c6ce3bf6ced8f
Leo <leo_1517@outlook.com>
2022-04-28 23:36:08 -0700

    Updated script

--- Assets/Leo/Scripts/NPCs/CheckCurrentPortrait.cs
+++ Assets/Leo/Scripts/NPCs/CheckCurrentPortrait.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckCurrentPortrait : MonoBehaviour
+public class CheckCurrentPortrait
 {
+    public Sprite[] portraits;
     public Sprite[] sprites;
 }
