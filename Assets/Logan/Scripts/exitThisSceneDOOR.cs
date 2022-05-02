d8daf169453e930d83364fb3e6908a140517dc93
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2022-05-02 10:22:58 -0700

    fxgnfgukmgjgjy  forgor 💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀

---
+++ Assets/Logan/Scripts/exitThisSceneDOOR.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitThisSceneDOOR : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 targetPos;
+    public Vector2 spawnLocation;
+    public string sceneToLoad;
+    public float speed;
+    [HideInInspector]
+    public Animator playerAni;
+    public Animator fadeAni;
+    private bool calledCoroutine;
+    public string playerDirection;
+    public static bool exitingScene;
+    bool canLeaveScene;
+
+    private void Awake()
+    {
+        playerAni = player.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(canLeaveScene == true && Input.GetKey(KeyCode.E))
+        {
+            //Debug.Log("GO");
+            exitingScene = true;
+            playerMovement.inLoadingZone = true;
+            if (calledCoroutine == false)
+            {
+                calledCoroutine = true;
+                StartCoroutine(ExitScene());
+                StartCoroutine(FadeAndSpawn());
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player") && EnterThisScene.enteringScene == false)
+        {
+            canLeaveScene = true;
+        }
+
+    }
+
+    public IEnumerator ExitScene()
+    {
+        while (player.transform.position != targetPos)
+        {
+            //Debug.Log("move");
+            SetDirection();
+            player.transform.position = Vector2.MoveTowards(player.transform.position, targetPos, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeAndSpawn()
+    {
+        fadeAni.SetTrigger("FadeTrigger");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneToLoad);
+        calledCoroutine = false;
+        playerMovement.inLoadingZone = false;
+        PlayerManager.spawnPoint = spawnLocation;
+        exitingScene = false;
+    }
+
+    private void SetDirection()
+    {
+        switch (playerDirection)
+        {
+            case "Left":
+                playerAni.SetBool("isMoving", true);
+                playerAni.SetBool("Left", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+            case "Right":
+                playerAni.SetBool("isMoving", true);
+                playerAni.SetBool("Right", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+            case "Up":
+                playerAni.SetBool("isMoving", true);
+                playerAni.SetBool("Up", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+            case "Down":
+                playerAni.SetBool("isMoving", true);
+                playerAni.SetBool("Down", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+            case "DownIdle":
+                playerAni.SetBool("isMoving", false);
+                playerAni.SetBool("Down", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+            case "UpIdle":
+                playerAni.SetBool("isMoving", false);
+                playerAni.SetBool("Up", true);
+                playerAni.SetBool("isCarrying", false);
+                break;
+        }
+    }
+}
