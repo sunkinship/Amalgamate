832fdf5991d3dfd371f5a935637387ff8eed1276
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2022-05-05 09:57:18 -0700

    Splash Screen

---
+++ Assets/Logan/Scripts/splashScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Animations;
+using UnityEngine.SceneManagement;
+
+public class splashScreen : MonoBehaviour
+{
+    public Animator textAnimator;
+    public Animator titleAnimator;
+    public Animator presentsAnimator;
+    public Image splashLogo;
+    public Image splashText;
+    public Image splashTitle;
+
+    private void Awake()
+    {
+        StartCoroutine(splashScreenRoutine());
+    }
+
+    public IEnumerator splashScreenRoutine()
+    {
+        presentsAnimator.SetTrigger("startShowLogo");
+        yield return new WaitForSeconds(2);
+        textAnimator.SetTrigger("startShowText");
+        yield return new WaitForSeconds(2);
+        presentsAnimator.SetTrigger("startHideLogo");
+        textAnimator.SetTrigger("startHideText");
+        titleAnimator.SetTrigger("startShowTitle");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("NewMainMenu");
+        yield return null;
+    }
+
+}
